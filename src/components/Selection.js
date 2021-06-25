f05e5f43e1f4e2301fd9b258671cdbf118a3ce56
sid-bhatt <70953578+sid-bhatt@users.noreply.github.com>
2021-06-25 13:19:40 +0530

    edit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React, { useState } from "react";
 
+const Selection = (props) => {
+  const { applyColor } = props;
+  const [selectStyle, updateSelectStyle] = useState({ background: "" });
+  return (
+    <div
+      className="box"
+      style={selectStyle}
+      onClick={() => {
+        applyColor(updateSelectStyle);
+      }}
+    >
+      <h3>Selection Page</h3>
+    </div>
+  );
+};
+
+export default Selection;
